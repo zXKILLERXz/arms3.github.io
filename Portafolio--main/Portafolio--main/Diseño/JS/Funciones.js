db18d8cd0888129c8ec9d1b5a3d449aa465407c6
zxkillerxz <126014205+zXKILLERXz@users.noreply.github.com>
2023-11-02 11:15:10 -0500

    add to portafolio

---
+++ Portafolio--main/Portafolio--main/Diseño/JS/Funciones.js
@@ -0,0 +1,26 @@
+/*         
+function openTab(tabName) {
+    const tabs = document.getElementsByClassName('tab');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';s
+        }
+    document.getElementById(tabName).style.display = 'block';
+        }
+    */
+function Proyectos() {
+    var proyecto = 'Proyectos.html';
+        window.open("C:/Portafolio/Diseño/Proyectos.html", '_blank');
+    }               
+    function Sobremi() {
+        var sobremi = 'Sobremi.html';
+        window.open("C:/Portafolio/Diseño/Sobremi.html", '_blank');
+    }   
+    function Contacto() {
+        var proyecto = 'Proyectos.html';
+        window.open("C:/Portafolio/Diseño/Contacto.html", '_blank');
+    }  
+    function Educacion() {
+        var Educacion = 'Educacion.html';
+        window.open("C:/Portafolio/Diseño/Educacion.html", '_blank');
+    } 
+   
